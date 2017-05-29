244015cc12c4f80e382d9d4ab71dc2201e032bd7
renanQuadri <renan.aguiar.o@gmail.com>
2017-05-29 13:04:52 -0300

    Initial commit

--- server.js
+++ server.js
@@ -1,145 +1,95 @@
-//  OpenShift sample Node application
-var express = require('express'),
-    fs      = require('fs'),
-    app     = express(),
-    eps     = require('ejs'),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
+var express = require('express')
+  , http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server, {'heartbeat interval': 5, 'heartbeat timeout' : 10});
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+      var msg = {
+                'id': '',
+                'user': '',
+                'room': '',
+                'text': '',
+                'time': '',
+                'advice': true
+            };
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
+io.on('connection', function(socket){
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
 
-  }
-}
-var db = null,
-    dbDetails = new Object();
+    socket.on('join:room', function(data){
+        var room_name = data.room_name;
+        socket.join(room_name);
+       
+        socket.room = room_name;
+        socket.user = data.user;
+        
+        data.text = data.user + " entrou no chat";
 
-var initDb = function(callback) {
-  if (mongoURL == null) return;
+        socket.in(socket.room).emit('entrou', data);
+    /*    socket.broadcast.to(socket.room).emit('qtd', io.sockets.adapter.rooms['Chat'].length);*/
+        io.in(socket.room).emit('qtd', io.sockets.adapter.rooms['Chat'].length);
 
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
-
-app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+        console.log(data.user +" entrou no " +room_name);
     });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});
 
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
-
-
-
-
-// Servidor: app.js
- // Iniciando servidor HTTP
-
- var server = require('http').createServer(index)
-   , io = require('socket.io').listen(server)
-   , fs = require('fs')
- ;
-/* app.listen(3000, function() {
-   console.log("Servidor rodando!");
- });*/
+/*    socket.on('get:users', function(data){
  
- function index(req, res){
-   fs.readFile(__dirname + '/index.html', function(err, data){
- res.writeHead(200);
-     res.end(data);
-   });
- };
- // Iniciando Socket.IO
- var visitas = 0;
- // Evento connection ocorre quando entra um novo usuário.
- io.on('connection', function(socket){
-   // Incrementa o total de visitas no site.
-   visitas++;
-   // Envia o total de visitas para o novo usuário.
-   socket.emit('visits', visitas);
-   // Envia o total de visitas para os demais usuários.
-   socket.broadcast.emit('visits', visitas);
-   // Evento disconnect ocorre quando sai um usuário.
-   socket.on('disconnect', function(){
-     visitas--;
-     // Atualiza o total de visitas para os demais usuários.
-     socket.broadcast.emit('message', visitas);
-   });
- });
+        socket.in(socket.room).emit('qtd', io.sockets.adapter.rooms['Chat'].length);
+  
+    });*/
+
+    socket.on('leave:room', function(msg){
+        msg.text = msg.user + " saiu do chat";
+        socket.leave(socket.room);
+        if(io.sockets.adapter.rooms['Chat']){
+            var teste = io.sockets.adapter.rooms['Chat'].length;
+        }else{
+            var teste = 0;
+        }
+        socket.in(msg.room).emit('exit', msg);
+        
+        socket.in(msg.room).emit('qtd', teste);
+
+         console.log(msg.user +" saiu do " + msg.room);
+    });
+
+
+    socket.on('send:message', function(msg){
+        socket.in(msg.room).emit('message', msg);
+
+        
+        /*io.sockets.clients('Mapa Um').length*/
+    });
+
+      socket.on('disconnect', function() {
+        msg.text = socket.user + " saiu do chat";
+        socket.leave(socket.room);
+        if(io.sockets.adapter.rooms['Chat']){
+            var teste = io.sockets.adapter.rooms['Chat'].length;
+        }else{
+            var teste = 0;
+        }
+        socket.in(socket.room).emit('exit', msg);
+        io.in(socket.room).emit('qtd', teste );
+
+  console.log(socket.user +" saiu do " + socket.room);
+        
+     /*   socket.broadcast.to(socket.room).emit('saiuMapa', socket.room, socket.username);
+        socket.emit('disconnectCanvas', socket.room);*/
+
+        /*delete infoGame[socket.room].players[socket.username];*/
+         
+        /*  io.sockets.in(socket.room).emit('playerReady', socket.username, false);*/
+        /*console.log("DISCONNECT: ", --clientsConnected);*/
+        // Remove o username da lista global de usernames
+
+        /*  if (!(socket.room == null)) socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username + ' saiu da sala');*/
+
+      });
+
+
+});
+
+/*server.listen(8100, "127.0.0.1");*/
+server.listen(8080, "127.0.0.1");
