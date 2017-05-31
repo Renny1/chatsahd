9666800e734fd357fc4b6c258863e87b6392b58d
renanQuadri <renan.aguiar.o@gmail.com>
2017-05-31 17:36:01 -0300

    Initial commit

--- server.js
+++ server.js
@@ -109,18 +109,72 @@ console.log('Server running on 2 http://%s:%s', ip, port);
 
 module.exports = app ;
 
-console.log('teste 2');
+      var msg = {
+                'id': '',
+                'user': '',
+                'room': '',
+                'text': '',
+                'time': '',
+                'advice': true
+            };
 
 io.on('connection', function(socket){
 
 
-
-      socket.on('disconnect', function() {
-
-      });
+socket.on('connect', function() {
+console.log("Alguem Conectou 1");
 
 });
-console.log('teste 3');
-//server.listen(8080, "127.0.0.1");
 
-console.log('teste 4');
+   socket.on('join:room', function(data){
+      console.log("Alguem Conectou 2");
+        var room_name = data.room_name;
+        socket.join(room_name);
+        socket.room = room_name;
+        socket.user = data.user;
+        data.text = data.user + " entrou no chat";
+        socket.in(socket.room).emit('entrou', data);
+        io.in(socket.room).emit('qtd', io.sockets.adapter.rooms['Chat'].length);
+        console.log(data.user +" entrou no " +room_name);
+
+    });
+
+    socket.on('leave:room', function(msg){
+        msg.text = msg.user + " saiu do chat";
+        socket.leave(socket.room);
+        if(io.sockets.adapter.rooms['Chat']){
+            var teste = io.sockets.adapter.rooms['Chat'].length;
+        }else{
+            var teste = 0;
+        }
+        socket.in(msg.room).emit('exit', msg);
+        
+        socket.in(msg.room).emit('qtd', teste);
+
+         console.log(msg.user +" saiu do " + msg.room);
+    });
+
+
+    socket.on('send:message', function(msg){
+        socket.in(msg.room).emit('message', msg);
+    });
+
+      socket.on('disconnect', function() {
+        msg.text = socket.user + " saiu do chat";
+        socket.leave(socket.room);
+        if(io.sockets.adapter.rooms['Chat']){
+            var teste = io.sockets.adapter.rooms['Chat'].length;
+        }else{
+            var teste = 0;
+        }
+        socket.in(socket.room).emit('exit', msg);
+        io.in(socket.room).emit('qtd', teste );
+        console.log(socket.user +" saiu do " + socket.room);
+      });
+
+
+
+
+});
+console.log('Rodando');
+//server.listen(8080, "127.0.0.1");
