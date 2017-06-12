67de5f7903811d77143c16e1af58e9fa6d7eb05e
renanQuadri <renan.aguiar.o@gmail.com>
2017-06-12 17:52:58 -0300

    Initial commit3

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ var SampleApp = function() {
     var self = this;
     self.setupVariables = function() {
         //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP || 0.0.0.1;
         self.port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 
         if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  Log errors on OpenShift but continue w/ 0.0.0.1 - this
             //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 0.0.0.1');
+            self.ipaddress = "0.0.0.1";
         };
     };
 
