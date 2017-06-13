b74ac2153bcf185291475ed21955ff093a2a9833
renanQuadri <renan.aguiar.o@gmail.com>
2017-06-13 17:16:28 -0300

    Initial commit3

--- server.js
+++ server.js
@@ -122,6 +122,8 @@ initDb(function(err){
 
 	            socket.on('join:room', function(data) {
 
+	            	console.log("teste");
+
 	            });
 
 
@@ -135,6 +137,5 @@ initDb(function(err){
 
 /*app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
 */
+module.exports = app ;
